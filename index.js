417c35878c71a65c7b0f593e63d02bf9dcfdbf41
songmingzhao <songmz@songmingzhaodeMacBook-Pro.local>
2017-06-09 15:22:42 +0800

    release 1.0

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const loaderUtils = require("loader-utils");
 module.exports = function (source, map) {
   this.cacheable && this.cacheable();
   const query = loaderUtils.getOptions(this) || {};
-
   for (let i = 0; i < query.functionArray.length; i++) {
     const functionName = query.functionArray[i].functionName;
     const functionBody = query.functionArray[i].functionBody;
+    const functionArgs = query.functionArray[i].functionArgs;
     const searchFuncRegExp = new RegExp(functionName + '\(.*\)', 'g');
-    console.log('functionBody', functionBody)
-    const func = new Function(functionBody);
+    const func = new Function(functionArgs,functionBody);
     source = source.replace(searchFuncRegExp, rv => {
       console.log(rv);
       const param = rv.match(/(\'|\").*(\'|\")/)[0].replace(/\'|\"/g, '');
       return func(param);
     })
   }
-  console.log(source)
   this.callback(null, source, map);
 }
