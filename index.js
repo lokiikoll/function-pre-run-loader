da346c6155feb736467cbb027e46f4d52b69436a
songmingzhao <songmz@songmingzhaodeMacBook-Pro.local>
2017-06-09 15:22:10 +0800

    complete 1.0

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const loaderUtils = require("loader-utils");
-module.exports = function (content) {
+module.exports = function (source, map) {
   this.cacheable && this.cacheable();
   const query = loaderUtils.getOptions(this) || {};
-  var limit = (this.options && this.options.url && this.options.url.dataUrlLimit) || 0;
-  if (query.limit) {
-    limit = parseInt(query.limit, 10);
+
+  for (let i = 0; i < query.functionArray.length; i++) {
+    const functionName = query.functionArray[i].functionName;
+    const functionBody = query.functionArray[i].functionBody;
+    const searchFuncRegExp = new RegExp(functionName + '\(.*\)', 'g');
+    console.log('functionBody', functionBody)
+    const func = new Function(functionBody);
+    source = source.replace(searchFuncRegExp, rv => {
+      console.log(rv);
+      const param = rv.match(/(\'|\").*(\'|\")/)[0].replace(/\'|\"/g, '');
+      return func(param);
+    })
   }
-  var mimetype = query.mimetype || query.minetype || mime.lookup(this.resourcePath);
-  if (limit <= 0 || content.length < limit) {
-    return "module.exports = " + JSON.stringify("data:" + (mimetype ? mimetype + ";" : "") + "base64," + content.toString("base64"));
-  } else {
-    var fileLoader = require("file-loader");
-    return fileLoader.call(this, content);
-  }
+  console.log(source)
+  this.callback(null, source, map);
 }
